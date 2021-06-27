e0e99da6b4154262bc85230f6b55236949acf27c
Aditi3 <aditi.gvc@gmail.com>
2021-06-27 09:04:05 +0530

    Update to City View Controller

--- DailyWeather/DailyWeather/Controller/CityViewController.swift
+++ DailyWeather/DailyWeather/Controller/CityViewController.swift
@@ -7,51 +7,56 @@
 
 import UIKit
 
-class EnterCityViewController: UIViewController {
+class CityViewController: UIViewController {
     
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var appNameLabel: UILabel!
     
-    var index = 0.0
+    /// [This property is] to update the index counter, we will using for App Name Animation
+    private var index = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        cityTextField.delegate = self
+        /// Conform Text Field Delegate to self
+        self.cityTextField.delegate = self
+        /// Add Animation to the App Name Display
         performAppNameLabelAnimation()
     }
     
     func performAppNameLabelAnimation() {
-        let appName = "DailyWeather"
-        for letter in appName {
+        for letter in K.appName {
             Timer.scheduledTimer(withTimeInterval: 0.1 * index, repeats: false) { timer in
+                /// Append the App Name Letters
                 self.appNameLabel.text?.append(letter)
             }
             self.index += 1
         }
     }
     
-    // MARK: Segue Methods
+    // MARK: - Segue Methods
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goToWeather" {
+        if segue.identifier == K.weatherPage {
             if let weatherVC = segue.destination as? CityWeatherViewController {
+                /// This passes the city name to the Weather View Controller
                 weatherVC.cityName = cityTextField.text
             }
         }
     }
     
-    // MARK: Actions
+    // MARK: - Actions
     
     @IBAction func buttonGoTapped(_ sender: Any) {
-        self.performSegue(withIdentifier: "goToWeather", sender: self)
+        self.performSegue(withIdentifier: K.weatherPage, sender: self)
     }
 }
 
 // MARK: - UITextFieldDelegate
 
-extension EnterCityViewController: UITextFieldDelegate {
+extension CityViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        /// Hides the TextField 
         textField.resignFirstResponder()
         return true
     }
--- DailyWeather/DailyWeather/Utils/Constants.swift
+++ DailyWeather/DailyWeather/Utils/Constants.swift
@@ -8,4 +8,7 @@
 struct K {
     static let weatherApiKey = "64aca4e110e5a524cc13faaaf78d909f"
     static let defaultCity = "Mumbai"
+    static let appName = "DailyWeather"
+    static let weatherPage = "goToWeather"
+
 }
