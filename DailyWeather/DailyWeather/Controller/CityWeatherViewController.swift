f0c0981776f0fb0f0e2f84bc3e0332ee2dfd5576
Aditi3 <aditi.gvc@gmail.com>
2021-06-27 09:42:07 +0530

    Refactor City Weather View Controller

--- DailyWeather/DailyWeather/Controller/CityWeatherViewController.swift
+++ DailyWeather/DailyWeather/Controller/CityWeatherViewController.swift
@@ -11,21 +11,27 @@ class CityWeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var pressureLabel: UILabel!
     @IBOutlet weak var windLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
-    @IBOutlet weak var weatherDesc: UILabel!
+    @IBOutlet weak var weatherDescLabel: UILabel!
     
     var cityName: String?
-    var weatherManager = WeatherManager()
+    private var weatherManager = WeatherManager()
     
+    
+    // MARK: - Controller Life Cycle Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         weatherManager.delegate = self
+        /// Request to fetch the Weather Details
         requestCityWeatherDetails()
     }
+    
+    // MARK: - Request Weather Details for the City
     
     func requestCityWeatherDetails() {
         if self.cityName == "" {
@@ -39,14 +45,17 @@ class CityWeatherViewController: UIViewController {
 // MARK: - WeatherManagerDelegate
 
 extension CityWeatherViewController: WeatherManagerDelegate {
-   
+    
+    /// Defining this Weather Manager method makes ViewController conform/adopt
+    /// the WeatherManagerDelegate protocol. This method is called
+    /// when the weather detail is fetched successfully.
     func didUpdateWeather(weatherManager: WeatherManager, weather: WeatherModel) {
         let weather = weather as WeatherModel
         DispatchQueue.main.async {
-            
+            /// Set View Elements, temperature, city name, description, humidity,  pressure and condition Image
             self.temperatureLabel.text = weather.temperatureString + "°"
-            self.cityLabel.text = weather.cityName
-            self.weatherDesc.text = weather.weatherDescription
+            self.cityNameLabel.text = weather.cityName
+            self.weatherDescLabel.text = weather.weatherDescription
             self.windLabel.text = weather.windString + "km/h"
             self.humidityLabel.text = weather.humidity.description + "%"
             self.pressureLabel.text = weather.pressure.description + "mb"
@@ -54,6 +63,9 @@ extension CityWeatherViewController: WeatherManagerDelegate {
         }
     }
     
+    /// Defining this Weather Manager method makes ViewController conform/adopt
+    /// the WeatherManagerDelegate protocol. This method is called
+    /// when the weather detail is fetched request is failed.
     func didFailWithError(error: Error) {
         print("Error handling data \(error)")
     }
